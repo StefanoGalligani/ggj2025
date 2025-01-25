a59832ef4ad61f43614a6348529c96e65533dbcb
StefanoGalligani <stefanogalligani0987@gmail.com>
2025-01-25 15:25:04 +0100

    Fixed powerups

--- Assets/Scripts/PowerupScript.cs
+++ Assets/Scripts/PowerupScript.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class PowerupScript : MonoBehaviour
 {
-    public AbstractBubble powerBubble { get; private set; }
+    [SerializeField] public AbstractBubble powerBubble;
 }
