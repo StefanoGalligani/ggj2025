dba577086e3def8b890974a43a044e9d1d8ec6ec
StefanoGalligani <stefanogalligani0987@gmail.com>
2025-01-25 16:27:36 +0100

    More generic powerup logic

--- Assets/Scripts/PowerupScript.cs
+++ Assets/Scripts/PowerupScript.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class PowerupScript : MonoBehaviour
 {
     [SerializeField] public AbstractBubble powerBubble;
+    [SerializeField] public PowerupType powerupType;
+}
+
+public enum PowerupType {
+    None,
+    SpecialBubble,
+    BubbleRaffic,
+    CannonLock
 }
