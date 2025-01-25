191a3cd29a2194cfe470169e4a3253870287ac7a
StefanoGalligani <stefanogalligani0987@gmail.com>
2025-01-25 22:30:48 +0100

    Fish and coral logic

---
+++ Assets/Scripts/FishScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{
+    [SerializeField] private Vector2 _bounds;
+    [SerializeField] private float _velocity;
+    private bool _facingRight;
+
+    private void Start() {
+        _facingRight = Random.Range(0,2) == 0;
+    }
+
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().flipX = !_facingRight;
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * _velocity * (_facingRight ? 1 : -1), transform.position.y, 0);
+        if (transform.position.x > _bounds.y) {
+            _facingRight = false;
+        } else if (transform.position.x < _bounds.x) {
+            _facingRight = true;
+        }
+    }
+}
