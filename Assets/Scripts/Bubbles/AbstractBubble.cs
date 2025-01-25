89b9ec96edd4e898263fe151ac713bf5fa9c90a5
StefanoGalligani <stefanogalligani0987@gmail.com>
2025-01-25 11:08:10 +0100

    Bubble physics behaviour

--- Assets/Scripts/Bubbles/AbstractBubble.cs
+++ Assets/Scripts/Bubbles/AbstractBubble.cs
@@ -4,7 +4,16 @@ public abstract class AbstractBubble : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _velocity;
+    [SerializeField] private Vector2 _targetYVelocityRange;
     public void Shoot(Vector2 direction) {
         _rb.AddForce(_velocity * direction, ForceMode2D.Impulse);
+    }
+
+    public void FixedUpdate() {
+        _rb.linearVelocityX = _rb.linearVelocityX * (1 - Time.fixedDeltaTime);
+
+        float targetYVelocity = Mathf.Clamp(_rb.linearVelocityY, _targetYVelocityRange.x, _targetYVelocityRange.y);
+        float deltaYVelocity = targetYVelocity - _rb.linearVelocityY;
+        _rb.linearVelocityY += deltaYVelocity * Time.fixedDeltaTime; 
     }
 }
